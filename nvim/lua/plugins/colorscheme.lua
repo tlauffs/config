4be4d8c4243dbf0cab70f90961316cee29ec2a54
tlauffs <timlauffs@gmail.com>
2024-09-29 21:52:30 +0200

    added colorthemes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,25 +1,45 @@
-vim.g.gruvbox_baby_transparent_mode = 1
+local color_scheme = os.getenv("COLOR_SCHEME") or "catppuccin"
 
-return {
-	-- You can easily change to a different colorscheme.
-	-- Change the name of the colorscheme plugin below, and then
-	-- change the command in the config to whatever the name of that colorscheme is.
-	--
-	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-	
-	-- "luisiacc/gruvbox-baby",
-	-- config = true,
-	-- opts = {},
-	-- priority = 1000, 
-	{
-		"catppuccin/nvim", 
-		name = "catppuccin", 
-		priority = 1000,  -- Missing comma is added here
-		config = function()
-			require("catppuccin").setup({
-				transparent_background = true, 
-			})
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
+local plugins = {
+   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+   {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true,
+            })
+        end,
+    },
+    {
+        "luisiacc/gruvbox-baby",
+	  name ="gruvbox-baby",
+        priority = 1000,
+        config = function()
+            vim.g.gruvbox_baby_transparent_mode = 1  
+        end,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+        config = function()
+            require("kanagawa").setup({
+                transparent = true,  
+            })
+        end,
+    },
 }
+
+-- Apply the colorscheme based on the environment variable
+vim.defer_fn(function()
+    if color_scheme == "catppuccin" then
+        vim.cmd.colorscheme("catppuccin")
+    elseif color_scheme == "gruvbox" then
+        vim.cmd.colorscheme("gruvbox-baby")
+    elseif color_scheme == "kanagawa" then
+        vim.cmd.colorscheme("kanagawa")
+    end
+end, 0)
+
+
+return plugins
